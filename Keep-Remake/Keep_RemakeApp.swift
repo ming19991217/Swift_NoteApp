1e59e36795ca46958c8201ec05c8fe3b6f131df4
ming <47671793+ming19991217@users.noreply.github.com>
2024-10-17 20:16:32 +0800

    Initial Commit

---
+++ Keep-Remake/Keep_RemakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Keep_RemakeApp.swift
+//  Keep-Remake
+//
+//  Created by Ming on 2024/10/17.
+//
+
+import SwiftUI
+
+@main
+struct Keep_RemakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
